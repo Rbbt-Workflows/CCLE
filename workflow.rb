226db618933a83d3a236e418dc8a5f61e64fbb16
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2017-02-21 17:22:39 +0100

    nil

---
+++ workflow.rb
@@ -0,0 +1,18 @@
+require 'rbbt-util'
+require 'rbbt/workflow'
+
+Misc.add_libdir if __FILE__ == $0
+
+require 'rbbt/sources/CCLE'
+
+module CCLE
+  extend Workflow
+
+
+end
+
+#require 'CCLE/tasks/basic.rb'
+
+#require 'rbbt/knowledge_base/CCLE'
+#require 'rbbt/entity/CCLE'
+
