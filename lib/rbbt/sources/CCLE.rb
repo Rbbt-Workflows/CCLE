2cd5557b6a8e8f2e456a7959b439ae43129dbc90
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2017-10-19 14:22:52 +0200

    Update data source URLs

--- lib/rbbt/sources/CCLE.rb
+++ lib/rbbt/sources/CCLE.rb
@@ -14,7 +14,7 @@ module CCLE
 
 
   CCLE.claim CCLE.gene_CNV, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_20/CCLE_copynumber_byGene_2013-12-03.txt.gz?downloadff=true&fileId=17599"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/dna_copy_number/CCLE_copynumber_byGene_2013-12-03.txt.gz"
     tsv = TSV.open(url, :wget_options => {"--no-check-certificate" => true}, :gzip => true, :type => :list, :header_hash => '')
     fields = tsv.fields
     tsv.key_field = "Entrez Gene ID"
@@ -24,7 +24,7 @@ module CCLE
   end
 
   CCLE.claim CCLE.gene_expression, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_21/CCLE_Expression_Entrez_2012-09-29.gct?downloadff=true&fileId=6763"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/mRNA_expression/CCLE_Expression_Entrez_2012-09-29.gct"
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     io2 = CMD.cmd('tail -n +3 | cut -f 2-', :in => io, :pipe => true)
     tsv = TSV.open(io2, :type => :list, :merge => true, :header_hash => '', :cast => :to_f)
@@ -33,14 +33,14 @@ module CCLE
   end
 
   CCLE.claim CCLE.cell_lines, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_22/CCLE_sample_info_file_2012-10-18.txt?downloadff=true&fileId=6801"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/cell_line_annotations/CCLE_sample_info_file_2012-10-18.txt"
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     tsv = TSV.open(io, :type => :list, :header_hash => '')
     tsv.to_s
   end
 
   CCLE.claim CCLE.oncomap_maf, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_23/CCLE_Oncomap3_2012-04-09.maf?downloadff=true&fileId=3000"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/oncomap_mutations/CCLE_Oncomap3_2012-04-09.maf"
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     tsv = TSV.open(io, :type => :double, :header_hash => '', :merge => true)
     tsv.key_field = "Associated Gene Name"
@@ -56,21 +56,22 @@ module CCLE
   end
 
   CCLE.claim CCLE.drugs, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_27/CCLE_NP24.2009_profiling_2012.02.20.csv?downloadff=true&fileId=3422"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/pharmacological_profiling/CCLE_NP24.2009_profiling_2012.02.20.csv"
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     tsv = TSV.open(io, :type => :list, :sep => ',', :header_hash => '', :fix => Proc.new{|l| l.gsub(/"[^"]+"/){|p| p.gsub(', ',"|").gsub('"','')}} )
     tsv.to_s
   end
 
   CCLE.claim CCLE.drug_profiles, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_27/CCLE_NP24.2009_Drug_data_2015.02.24.csv?downloadff=true&fileId=20777"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/pharmacological_profiling/CCLE_NP24.2009_Drug_data_2015.02.24.csv"
+
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     tsv = TSV.open(io, :type => :double, :merge => true, :sep => ',', :header_hash => '', :fix => Proc.new{|l| l.gsub(/"[^"]+"/){|p| p.gsub(',',"|").gsub('"','')}} )
     tsv.to_s
   end
 
   CCLE.claim CCLE.cell_line_binary, :proc do  |filename|
-    url = "https://portals.broadinstitute.org/ccle/downloadFile/DefaultSystemRoot/exp_10/ds_30/CCLE_MUT_CNA_AMP_DEL_binary_Revealer.gct?downloadff=true&fileId=24742"
+    url = "https://data.broadinstitute.org/ccle_legacy_data/binary_calls_for_copy_number_and_mutation_data/CCLE_MUT_CNA_AMP_DEL_binary_Revealer.gct"
     io = Open.open(url, :wget_options => {"--no-check-certificate" => true})
     io2 = CMD.cmd('tail -n +3 | cut -f 2-', :in => io, :pipe => true)
     tsv = TSV.open(io2, :type => :list, :header_hash => '', :cast => :to_i)
